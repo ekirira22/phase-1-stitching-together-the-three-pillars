4cb010d7b708cd0959d7f996f6260b215e52b354
Eric Maranga <ekirira22@gmail.com>
2024-01-04 13:42:39 +0300

    changes to practice branch

--- demo.js
+++ demo.js
@@ -14,51 +14,45 @@ const colorStates = {
 // we want to make clickable. Without JavaScript, clicking on these heart shapes
 // does nothing. Uncomment the code and refresh the demo page. 
 
-const articleHearts = document.querySelectorAll(".like-glyph");
-console.log(articleHearts)
+//ALGO
+//1. Fetch the nodes that you want the changes on
+//2. Loop through each node, which attaching an event listener
+//3. Create a function that will effect the change
 
-function likeCallback(e) {
-  const heart = e.target;
-  console.log(heart)
-  mimicServerCall()
-    .then(function(serverMessage){
-      // STEP 2: Uncomment the 3 lines after the alert.
-      // Here we're using Pillar 1 (DOM Manipulation) to update the screen and
-      // mimicking Pillar 3 (Server Communication) to only update the screen if
-      // the sending of information to the server succeeds.
-      alert("You notified the server!");
-      alert(serverMessage);
-      heart.innerText = glyphStates[heart.innerText];
-      heart.style.color = colorStates[heart.style.color];
-    })
-    .catch(function(error) {
-      alert("Something went wrong!");
-    });
+const heartglyphs = document.querySelectorAll('.like-glyph')
+console.log(heartglyphs)
+
+for (const glyph of heartglyphs){
+  glyph.addEventListener("click", likeUnlikeFunction);
 }
 
-// STEP 3: In order for the call to the server and the update of the screen to
-// work, we need to add a click event listener to the elements we identified in
-// STEP 1. That's Pillar 2, event handling. Uncomment this code:
+function likeUnlikeFunction(e){
+  const hearts = e.target
+  console.log(hearts)
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert("This means server was alerted")
+      alert(serverMessage)
 
-    for (const glyph of articleHearts) {
-      glyph.addEventListener("click", likeCallback);
-    }
+      hearts.innerText = glyphStates[hearts.innerText]
+      hearts.style.color = colorStates[hearts.style.color]
 
-// STEP 4: 
+    })
+    .catch(error => {
+      alert("Something went wrong")
+    })
+}
 
-// When all the STEPs' code changes have been complete, you'll be able to see a
-// working demonstration of our reference example. Sure, it's maybe not as
-// pretty as a professional site, but they're only different from our site in
-// style, not substance.
 
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
+
+
+
+
 
 function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       resolve("Pretend remote server notified of action!");
-    }, 300);
+    }, 300)
   });
 }
